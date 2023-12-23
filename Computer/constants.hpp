d827eb439664ee7bffc517e1bcbc972336b5162a
morrrrrrrr <moritz.weissenboeck@gmx.at>
2023-12-23 11:44:48 +0100

    Add vector.hpp from morrrrrrrr/vector.hpp repo

--- Computer/constants.hpp
+++ Computer/constants.hpp
@@ -1,7 +1,11 @@
 
+#include "vector.hpp"
+
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
 using uint8_t = unsigned char;
+using Vector3f = vector::Vector<3, float>;
+using Vector2f = vector::Vector<2, float>;
 
 #endif
--- Computer/leg.hpp
+++ Computer/leg.hpp
@@ -6,6 +6,7 @@
  */
 
 #include "constants.hpp"
+#include "vector.hpp"
 #include <tuple>
 #include <cmath>
 
