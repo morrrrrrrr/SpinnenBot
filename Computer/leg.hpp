9d19dd63fd9cb56d104fbe41befb0880f0fadba5
morrrrrrrr <moritz.weissenboeck@gmx.at>
2023-12-23 12:31:12 +0100

    Removed old leg class

--- Computer/leg.hpp
+++ Computer/leg.hpp
@@ -13,7 +13,7 @@
 
 #define M_PI 3.14159265358979323846 /* pi */
 
-class Leg_rewrite {
+class Leg {
     uint8_t m_index;
     
     // in which angle does the local coordinate system stand to the global
@@ -28,7 +28,7 @@ class Leg_rewrite {
 
 public:
     // Simple Constructor
-    Leg_rewrite(uint8_t index, float angle, Vector3f axis_lengths, Vector3f servo_offsets) : 
+    Leg(uint8_t index, float angle, Vector3f axis_lengths, Vector3f servo_offsets) : 
         m_index(index), m_angle(angle), m_axis_lengths(axis_lengths), m_servo_offsets(servo_offsets), m_target_point({0, 0, 0}) {  }
 
     // Set the target position of the leg (input point is in global coordinates)
@@ -76,73 +76,6 @@ public:
     Vector3f calculate(const Vector3f& target) {
         setTarget(target);
         return calculate();
-    }
-};
-
-#include <tuple> // only the old leg is dependent on this
-class Leg {
-    uint8_t index;
-    float l0, l1, l2;
-    float s0Offset, s1Offset, s2Offset;
-    float x, y, z; // target X,Y,Z; relative pos: global angle already calculated
-    float angle;   // offset angle
-public:
-    Leg() {
-        this->index = 255;
-        this->angle = 0;
-        this->l0 = 0;
-        this->l1 = 0;
-        this->l2 = 0;
-        this->s0Offset = 0;
-        this->s1Offset = 0;
-        this->s2Offset = 0;
-    }
-    Leg(uint8_t index, float angle, float l0, float l1, float l2, float s0Offset, float s1Offset, float s2Offset) {
-        this->index = index;
-        this->angle = angle;
-
-        this->l0 = l0;
-        this->l1 = l1;
-        this->l2 = l2;
-
-        this->s0Offset = s0Offset;
-        this->s1Offset = s1Offset;
-        this->s2Offset = s2Offset;
-    }
-
-
-    void begin() {
-        
-    }
-    std::tuple<float, float, float> update() {
-        float x = std::sqrt(this->x*this->x + this->z*this->z) - l0;
-        float p0 = std::atan2(this->z, this->x); // Angle between local x and z -> angle for servo 0
-        float b = std::atan2(y, x); // Angle beta
-
-        float a = std::acos((l1*l1 + l2*l2 - x*x - y*y) / (2*l1*l2)); // Angle alpha between axises
-        float p2 = M_PI - a;
-
-        float psi = std::atan2(l2 * std::sin(p2), l1 + l2 * std::cos(p2));
-        float p1 = b + psi;
-
-        // p0 * 180 / M_PI + this->s0Offset;
-        // p1 * 180 / M_PI + this->s1Offset;
-        // p2 * 180 / M_PI + this->s1Offset;
-
-        return {
-            p0 * 180 / M_PI + this->s0Offset,
-            p1 * 180 / M_PI + this->s1Offset,
-            p2 * 180 / M_PI + this->s1Offset
-        };
-    }
-
-    // this automatically converts the global-space input vector to a local-space vector
-    void setPosition(float x, float y, float z) {
-        // rotate from global space to local space
-        float a = (angle - 90) * M_PI/180;
-        this->x =  x * cos(a) + z * sin(a);
-        this->y =  y;
-        this->z = -x * sin(a) + z * cos(a);
     }
 };
 
